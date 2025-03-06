a7237e93747900e25ecb3bd1de7e75b32ffe57e2
Durga sairam <durgasairam061@gmail.com>
2025-03-06 18:31:41 +0530

    first changes

--- src/TakesScreenshot.java
+++ src/TakesScreenshot.java
@@ -1,9 +1,15 @@
- 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class TakesScreenshot {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		WebDriver driver = new ChromeDriver();
+		
+		TakesScreenshot ts = (TakesScreenshot) driver;
+	
+       
 	}
 
 }
